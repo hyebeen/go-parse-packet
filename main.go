5b8b6d0cd9ff4b26c32e638a8638ad635ede88ef
hyebeen <llhb3015@naver.com>
2016-05-29 02:04:43 +0900

    test

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
-/*
-	특정 인터페이스로부터 암호화되지 않은 패킷을 읽은 뒤, 
-	민감한 정보를 파싱하는 프로그램.
-*/
-
-package main
+package goparsepacket
 
 import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello, playground")
+func Test1() {
+	fmt.Println("Test")
+}
+
+func Test2(s string) {
+	fmt.Println("Test", s)
+}
+
+func Test3(s string) string {
+	return "Test" + s
 }
