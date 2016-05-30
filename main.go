8d230bccb19a7e935c208f688ee1e79112005636
hyebeen <llhb3015@naver.com>
2016-05-30 20:48:38 +0900

    http cookie

--- main.go
+++ main.go
@@ -1,17 +1,53 @@
-package goparsepacket
+// package goparsepacket
+package main
 
 import (
 	"fmt"
+	"log"
+	"regexp"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
-func Test1() {
-	fmt.Println("Test")
+var (
+	device       string = "eth5"
+	snapshot_len int32  = 1024
+	promiscuous  bool   = false
+	err          error
+	timeout      time.Duration = 30 * time.Second
+	handle       *pcap.Handle
+)
+
+var (
+	cookieRe = regexp.MustCompile(`Cookie: (:?\w+=.+)+`)
+)
+
+func main() {
+	f()
 }
 
-func Test2(s string) {
-	fmt.Println("Test!!", s)
-}
+func f() {
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
 
-func Test3(s string) string {
-	return "Test" + s
+	// Use the handle as a packet source to process all packets
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		tcpLayer := packet.Layer(layers.LayerTypeTCP)
+		if tcpLayer != nil {
+			tcpPacket := tcpLayer.(*layers.TCP)
+			http := string(tcpPacket.Payload)
+
+			if s := cookieRe.FindString(http); len(s) > 0 {
+				fmt.Println(s)
+			}
+		}
+	}
 }
